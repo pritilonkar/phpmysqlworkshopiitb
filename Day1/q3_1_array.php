10afda63e4a616c58da0a1565c16d032532eae24
pritilonkar <72161268+pritilonkar@users.noreply.github.com>
2020-10-02 14:51:06 +0530

    Update q3_1_array.php

--- Day1/q3_1_array.php
+++ Day1/q3_1_array.php
@@ -1,9 +1,9 @@
 <?php
-$days = array( "0"=>"Sunday", "1"=>"Monday", "2"=>"Tuesday", "3"=>"Wednesday", "4"=>"Thursday", "5"=>"Friday","6"=>"Saturday");
+$week = array( "0"=>"Sunday", "1"=>"Monday", "2"=>"Tuesday", "3"=>"Wednesday", "4"=>"Thursday", "5"=>"Friday","6"=>"Saturday");
 
 
-foreach($days as $x => $val) {
-  echo "$x = $val<br>";
+foreach($week as $x => $value) {
+  echo "$x = $value<br>";
 }
 
 ?>
